d5069656e318558efb5f67f6386fb6c8b920614b
Puyan Wei <puyanwei@gmail.com>
2021-07-15 18:28:49 +0100

    Added Styled Components, removed css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { Button, Card, Radio } from "@material-ui/core";
 import { Checkbox1 } from "formComponents/Checkbox1";
 import { TextField1 } from "formComponents/TextField1";
 import { Field, Form, Formik } from "formik";
+import styled from "styled-components";
 import { validationSchema } from "validationSchema";
-import "./App.css";
 
 const App = () => {
   return (
@@ -20,55 +20,81 @@ const App = () => {
         onSubmit={(data) => console.log("submit", data)}
       >
         {({ values, errors }) => (
-          <Form>
-            <Card variant="outlined">
-              <br />
-              <div className="form-element">
-                <TextField1
-                  placeholder="First name"
-                  name="firstName"
-                  type="input"
-                />
-              </div>
-              <div className="form-element">
-                <TextField1 placeholder="Surname" name="surname" type="input" />
-              </div>
-              <div className="form-element">
-                <Checkbox1
-                  label="Do you have children?"
-                  name="hasChildren"
-                  type="checkbox"
-                />
-              </div>
-              <div className="radio-text">What is your gender?</div>
-              <div className="radio-container">
-                <Field
-                  name="gender"
-                  type="radio"
-                  value="male"
-                  as={Radio}
-                ></Field>
-                Male
-                <Field
-                  name="gender"
-                  type="radio"
-                  value="female"
-                  as={Radio}
-                ></Field>
-                Female
-              </div>
-              <div className="form-element">
-                <Button type="submit">submit</Button>
-              </div>
-              <br />
-              <br />
-              <pre>{JSON.stringify(values, null, 2)}</pre>
-              <pre>{JSON.stringify(errors, null, 2)}</pre>
-            </Card>
-          </Form>
+          <FormLayout>
+            <Form>
+              <Card variant="outlined">
+                <br />
+                <FormElementLayout>
+                  <TextField1
+                    placeholder="First name"
+                    name="firstName"
+                    type="input"
+                  />
+                </FormElementLayout>
+                <FormElementLayout>
+                  <TextField1
+                    placeholder="Surname"
+                    name="surname"
+                    type="input"
+                  />
+                </FormElementLayout>
+                <FormElementLayout>
+                  <Checkbox1
+                    label="Do you have children?"
+                    name="hasChildren"
+                    type="checkbox"
+                  />
+                </FormElementLayout>
+                <RadioTextLayout>What is your gender?</RadioTextLayout>
+                <RadioLayout>
+                  <Field
+                    name="gender"
+                    type="radio"
+                    value="male"
+                    as={Radio}
+                  ></Field>
+                  Male
+                  <Field
+                    name="gender"
+                    type="radio"
+                    value="female"
+                    as={Radio}
+                  ></Field>
+                  Female
+                </RadioLayout>
+                <FormElementLayout>
+                  <Button type="submit">submit</Button>
+                </FormElementLayout>
+                <br />
+                <br />
+                <pre>{JSON.stringify(values, null, 2)}</pre>
+                <pre>{JSON.stringify(errors, null, 2)}</pre>
+              </Card>
+            </Form>
+          </FormLayout>
         )}
       </Formik>
     </div>
   );
 };
+
+const FormLayout = styled.form`
+  width: 20rem;
+  margin: 2rem auto;
+`;
+
+const FormElementLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1rem;
+`;
+
+const RadioTextLayout = styled.div`
+  margin: 1rem 1rem 0.5rem 3.5rem;
+`;
+
+const RadioLayout = styled.div`
+  margin: 0.5rem 1rem 1rem 3rem;
+`;
+
 export default App;
--- src/formComponents/Checkbox1.jsx
+++ src/formComponents/Checkbox1.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useField } from "formik";
 import { Checkbox, FormControlLabel, FormHelperText } from "@material-ui/core";
 
@@ -5,9 +6,16 @@ export const Checkbox1 = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   const errorText = meta.error && meta.touched ? meta.error : "";
   return (
-    <>
+    <Layout>
       <FormControlLabel control={<Checkbox {...field} />} label={label} />
       <FormHelperText error={Boolean(errorText)}>{errorText}</FormHelperText>
-    </>
+    </Layout>
   );
 };
+
+const Layout = styled.div`
+  margin-left: 0.5rem;
+  * > {
+    display: block;
+  }
+`;
