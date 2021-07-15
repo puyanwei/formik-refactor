042c056ac6fbef36c39922a9a8632daed988fb60
Puyan Wei <puyanwei@gmail.com>
2021-07-15 17:59:30 +0100

    Added css to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Radio } from "@material-ui/core";
+import { Button, Card, Radio } from "@material-ui/core";
 import { Checkbox1 } from "formComponents/Checkbox1";
 import { TextField1 } from "formComponents/TextField1";
 import { Field, Form, Formik } from "formik";
@@ -21,44 +21,50 @@ const App = () => {
       >
         {({ values, errors }) => (
           <Form>
-            <div>
-              <TextField1
-                placeholder="First name"
-                name="firstName"
-                type="input"
-              />
-            </div>
-            <div>
-              <TextField1 placeholder="Surname" name="surname" type="input" />
-            </div>
-            <div>
-              <Checkbox1
-                label="Do you have children?"
-                name="hasChildren"
-                type="checkbox"
-              />
-            </div>
-            <div>
-              <div>What is your gender?</div>
-              <Field name="gender" type="radio" value="male" as={Radio}></Field>
-              Male
-              <Field
-                name="gender"
-                type="radio"
-                value="female"
-                as={Radio}
-              ></Field>
-              Female
-            </div>
-            <div>
-              <Button type="submit">submit</Button>
-            </div>
-            <br />
-            <br />
-            <br />
-            <br />
-            <pre>{JSON.stringify(values, null, 2)}</pre>
-            <pre>{JSON.stringify(errors, null, 2)}</pre>
+            <Card variant="outlined">
+              <br />
+              <div className="form-element">
+                <TextField1
+                  placeholder="First name"
+                  name="firstName"
+                  type="input"
+                />
+              </div>
+              <div className="form-element">
+                <TextField1 placeholder="Surname" name="surname" type="input" />
+              </div>
+              <div className="form-element">
+                <Checkbox1
+                  label="Do you have children?"
+                  name="hasChildren"
+                  type="checkbox"
+                />
+              </div>
+              <div className="radio-text">What is your gender?</div>
+              <div className="radio-container">
+                <Field
+                  name="gender"
+                  type="radio"
+                  value="male"
+                  as={Radio}
+                ></Field>
+                Male
+                <Field
+                  name="gender"
+                  type="radio"
+                  value="female"
+                  as={Radio}
+                ></Field>
+                Female
+              </div>
+              <div className="form-element">
+                <Button type="submit">submit</Button>
+              </div>
+              <br />
+              <br />
+              <pre>{JSON.stringify(values, null, 2)}</pre>
+              <pre>{JSON.stringify(errors, null, 2)}</pre>
+            </Card>
           </Form>
         )}
       </Formik>
